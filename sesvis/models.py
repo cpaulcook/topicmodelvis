5e8141e4eb3458542c369915237cefcd80301a5d
Paul <paulcook@sminket.(none)>
2013-02-23 00:02:59 +1100

    Reworked models and added some tests

--- sesvis/models.py
+++ sesvis/models.py
@@ -1,42 +1,68 @@
 from django.db import models
 
+def build_unicode(*l):
+    return ':'.join([unicode(x) for x in l])
+
 class Corpus(models.Model):
-    corpus_id = models.IntegerField()
     name = models.TextField()
     description = models.TextField()
 
+    def __unicode__(self):
+        return build_unicode((self.name))
+
 class SubCorpus(models.Model):
-    sub_corpus_id = models.IntegerField()
     name = models.TextField()
     corpus = models.ForeignKey(Corpus)
     description = models.TextField()
     
+    def __unicode__(self):
+        return build_unicode(self.corpus.name, self.name)
+
 class Topic(models.Model):
-    topic_id = models.IntegerField()
     corpus = models.ForeignKey(Corpus)
+
+    def __unicode__(self):
+        return build_unicode(self.corpus.name, self.id)
 
 class ProbWordGivenTopic(models.Model):
     topic = models.ForeignKey(Topic)
     word = models.TextField()
     prob = models.FloatField()
 
+    def __unicode__(self):
+        return build_unicode(self.topic.corpus.name, self.topic.id, self.word)
+
 class Document(models.Model):
-    doc_id = models.IntegerField()
     title = models.TextField()
     text = models.TextField()
     corpus = models.ForeignKey(Corpus)
+
+    def __unicode__(self):
+        return build_unicode(self.corpus.name, self.title)
 
 class ProbTopicGivenDoc(models.Model):
     topic = models.ForeignKey(Topic)
     document = models.ForeignKey(Document)
     prob = models.FloatField()
 
+    def __unicode__(self):
+        return build_unicode(self.document.corpus.name, self.document.title,
+                             self.topic.id)
+
 class SubCorpusContent(models.Model):
     subcorpus = models.ForeignKey(SubCorpus)
     document = models.ForeignKey(Document)
+
+    def __unicode__(self):
+        return build_unicode(self.subcorpus.corpus.name, self.subcorpus.name,
+                             self.document.title)
 
 class TokenLevelTopicAllocation(models.Model):
     topic = models.ForeignKey(Topic)    
     document = models.ForeignKey(Document)    
     token_id = models.TextField()
     word = models.TextField()
+
+    def __unicode__(self):
+        return build_unicode(self.topic.corpus.name, self.document.title, 
+                             self.topic.id, self.token_id, self.word)
--- sesvis/tests.py
+++ sesvis/tests.py
@@ -6,11 +6,45 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from sesvis.models import *
 
 class SimpleTest(TestCase):
+    c = Corpus(name='try', description="try corpus")
+    sc = SubCorpus(name='trysc', corpus=c, description='try sc')
+    t = Topic(corpus=c)
+    pwgt = ProbWordGivenTopic(topic=t, word='blah', prob=0.53)
+    d = Document(title="mydoc", text="the text of a document...", corpus=c)
+    ptgd = ProbTopicGivenDoc(topic=t, document=d, prob=0.67)
+    scc = SubCorpusContent(subcorpus=sc, document=d)
+    tlta = TokenLevelTopicAllocation(topic=t, document=d, token_id=57, 
+                                     word='testsareawesome')
+
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def test_create_corpus(self):
+        assert unicode(SimpleTest.c) == u'try'
+
+    def test_create_subcorpus(self):
+        assert unicode(SimpleTest.sc) == u'try:trysc'
+
+    def test_create_topic(self):
+        assert unicode(SimpleTest.t) == u'try:None'
+
+    def test_create_probwordgiventopic(self):
+        assert unicode(SimpleTest.pwgt) == u'try:None:blah'
+        
+    def test_document(self):
+        assert unicode(SimpleTest.d) == u'try:mydoc'
+
+    def test_probtopicgivendoc(self):
+        assert unicode(SimpleTest.ptgd) == u'try:mydoc:None'
+    
+    def test_subcorpuscontent(self):
+        assert unicode(SimpleTest.scc) == u'try:trysc:mydoc'
+
+    def test_tokenleveltopicallocation(self):
+        assert unicode(SimpleTest.tlta) == u'try:mydoc:None:57:testsareawesome'
