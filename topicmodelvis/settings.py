4f314add62e6e11d4bd257a620decdd9dbac88e0
Karl Grieser <k.grieser@gmail.com>
2013-02-28 01:09:17 +1100

    Corrected a value I was using locally.

--- topicmodelvis/settings.py
+++ topicmodelvis/settings.py
@@ -20,7 +20,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\Karl\\git\\topicmodelvis-new\\topicmodel.db',                # Or path to database file if using sqlite3.
+        #'NAME': 'C:\\Users\\Karl\\git\\topicmodelvis-new\\topicmodel.db',                # Or path to database file if using sqlite3.
+        'NAME': 'topicmodel.db',
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
