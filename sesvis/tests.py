0daf5faf0bb064e0b824d60f7a89592c0af24ebc
Paul <paulcook@penguin>
2013-03-01 14:12:53 +1100

    Added tests

--- sesvis/tests.py
+++ sesvis/tests.py
@@ -24,6 +24,8 @@ class SimpleTest(TestCase):
         self.t.save()
         self.t2 = Topic(corpus=self.c, corpus_topic_id=4)
         self.t2.save()
+        self.t3 = Topic(corpus=self.c, corpus_topic_id=5)
+        self.t3.save()
 
         self.pwgt = ProbWordGivenTopic(topic=self.t, word='blah', prob=0.53)
         self.pwgt.save()
@@ -34,16 +36,24 @@ class SimpleTest(TestCase):
         self.d.save()
         self.d2 = Document(title="mydoc2", corpus=self.c)
         self.d2.save()
+        self.d3 = Document(title="mydoc3", corpus=self.c)
+        self.d3.save()
 
         self.doc_content = DocumentContent(document=self.d, 
                                            text="This is the content of a document...")
         self.doc_content.save()
         
-        self.ptgd = ProbTopicGivenDoc(topic=self.t, document=self.d, prob=0.4)
+        self.ptgd = ProbTopicGivenDoc(topic=self.t, document=self.d, prob=0.1)
         self.ptgd.save()
-        ProbTopicGivenDoc(topic=self.t2, document=self.d, prob=0.6).save()
-        ProbTopicGivenDoc(topic=self.t, document=self.d2, prob=0.1).save()
-        ProbTopicGivenDoc(topic=self.t2, document=self.d2, prob=0.9).save()
+        ProbTopicGivenDoc(topic=self.t2, document=self.d, prob=0.7).save()
+        ProbTopicGivenDoc(topic=self.t3, document=self.d, prob=0.2).save()
+        ProbTopicGivenDoc(topic=self.t, document=self.d2, prob=0.2).save()
+        ProbTopicGivenDoc(topic=self.t2, document=self.d2, prob=0.3).save()
+        ProbTopicGivenDoc(topic=self.t3, document=self.d2, prob=0.5).save()
+
+        ProbTopicGivenDoc(topic=self.t, document=self.d3, prob=0.1).save()
+        ProbTopicGivenDoc(topic=self.t2, document=self.d3, prob=0.5).save()
+        ProbTopicGivenDoc(topic=self.t3, document=self.d3, prob=0.4).save()
 
         self.scc = SubCorpusContent(subcorpus=self.sc, document=self.d)
         self.scc.save()
@@ -66,6 +76,10 @@ class SimpleTest(TestCase):
     def test_build_unicode(self):
         assert build_unicode('a','b','c') == u'a:b:c'
 
+    def test_get_subcorpus_access(self):
+        # Need to test this function
+        assert False
+
     def test_corpus_unicode(self):
         assert unicode(self.c) == u'try'
 
@@ -73,17 +87,31 @@ class SimpleTest(TestCase):
         assert unicode(self.sc) == u'try:trysc'
 
     def test_subcorpus_ave_prob_topic_given_doc_1(self):
-        assert self.sc.ave_prob_topic_given_doc() == {self.t:0.4,self.t2:0.6}
+        assert self.sc.ave_prob_topic_given_doc() == {self.t:0.1, self.t2:0.7,
+                                                      self.t3:0.2}
 
     def test_subcorpus_ave_prob_topic_given_doc_2(self):
-        print self.sc2.ave_prob_topic_given_doc()
-        assert self.sc2.ave_prob_topic_given_doc() == {self.t:0.25,self.t2:0.75}
+        result =  self.sc2.ave_prob_topic_given_doc()
+        expected = {self.t:0.15, self.t2:0.5, self.t3:0.35}
+        for t in expected:
+            self.assertAlmostEqual(expected[t], result[t])
 
-    # def test_subcorpus_best_k_topics(self):
-    #     ***** Working here *****
+    def test_subcorpus_best_k_topics(self):
+        assert self.sc2.best_k_topics(k=2) == [self.t2, self.t3]
 
     def test_topic_unicode(self):
         assert unicode(self.t) == u'try:1:3'
+
+    def test_topic_best_k_words(self):
+        assert self.t.best_k_words(k=2) == ['hannah', 'blah']
+
+    def test_topic_best_k_words_probs(self):
+        assert self.t.best_k_words(k=2, 
+                                   probs=True) == [('hannah',0.82), 
+                                                   ('blah',0.53)]
+
+    def test_topic_best_k_documents(self):
+        assert self.t3.best_k_documents(k=2) == [self.d2, self.d3]
 
     def test_probwordgiventopic_unicode(self):
         assert unicode(self.pwgt) == u'try:1:blah'
@@ -97,12 +125,6 @@ class SimpleTest(TestCase):
     def test_probtopicgivendoc_unicode(self):
         assert unicode(self.ptgd) == u'try:mydoc:1'
     
-    def test_probtopicgivendoc_best_k_words(self):
-        assert self.t.best_k_words(k=2) == ['hannah', 'blah']
-
-    def test_probtopicgivendoc_best_k_words_probs(self):
-        assert self.t.best_k_words(k=2, probs=True) == [('hannah',0.82), ('blah',0.53)]
-
     def test_subcorpuscontent_unicode(self):
         assert unicode(self.scc) == u'try:trysc:mydoc'
 
